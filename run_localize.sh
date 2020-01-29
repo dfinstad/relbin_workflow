c8c3c3e362b1488739be441406ee74c6ffe7db40
dfinstad <daniel.finstad@ligo.org>
2020-01-29 11:44:00 -0500

    add bayestar localization script

---
+++ run_localize.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Note: this needs to be run in a python 3.6 enironment with
+# the ligo-skymap package installed. LAL_DATA_PATH environment
+# variable must also be set to a valid lalsuite-extra location
+
+ANALYSIS_DIR=${1}
+OUTDIR=${ANALYSIS_DIR}/sky_localization/skymap_files
+CWD=${PWD}
+
+# IMPORTANT: HIGHLY RECOMMENDED IF USING A SHARED WORKSTATION.
+# Explicitly set the number of OpenMP threads
+# instead of using all available cores.
+export OMP_NUM_THREADS=4
+
+mkdir -p ${OUTDIR}
+cd ${OUTDIR}
+for FILE in ../ligolw_files/*.xml; do
+    # Run BAYESTAR
+    bayestar-localize-coincs ${FILE}
+    # rename output file from 0.fits
+    INJID=${FILE#../ligolw_files/ligolw_injection_}
+    INJID=${INJID%.xml}
+    OUTFILE=skymap_${INJID}.fits
+    mv 0.fits ${OUTFILE}
+done
+cd ${CWD}
